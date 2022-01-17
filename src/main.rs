6efeea24ec99b478f768c4ccab7d7c644fa6bf57
Adam Brailove <adam.brailove@gmail.com>
2022-01-16 16:22:53 -0800

    Got reading of api price data working

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,39 @@
-fn main() {
-    let v = vec![1.0, 2.0, 4.0, 3.2, 0.5, 4.2, 7.7, 8.3];
+//use reqwest::Error;
+use serde::{Deserialize, Serialize};
 
-    let result = signal_change(v);
-
-    for element in result {
-        println!("The value is {}", element)
-    }
+#[derive(Serialize, Deserialize, Debug)]
+struct KucoinResponse {
+    code: String,
+    data: Vec<[String; 7]>,
 }
 
-fn signal_change(v: Vec<f64>) -> Vec<f64> {
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let url = "https://api.kucoin.com/api/v1/market/candles?type=1min&symbol=BTC-USDT&startAt=1566703297&endAt=1566789757";
+    let resp_string: String = reqwest::blocking::get(url)?.text()?;
+    let deserialized: KucoinResponse = serde_json::from_str(&resp_string).unwrap();
+    println!("deserialized = {:?}", deserialized.code);
+    println!("deserialized = {:?}", deserialized.data[0][2]);
+    Ok(())
+}
+
+// Example kucoin price query. Max 1500 data points per response
+// https://api.kucoin.com/api/v1/market/candles?type=1min&symbol=BTC-USDT&startAt=1566703297&endAt=1566789757
+// json_response =
+//  {
+//      "code":"200000",
+//      "data":[
+//          [time_seconds, open, close, high, low, volume, turnover],
+//          ...
+//      ]
+//  }
+//
+
+fn _signal_change(v: Vec<f64>) -> Vec<f64> {
     let mut result_vec: Vec<f64> = Vec::new();
     let n = v.len();
-    let last_element = v[n - 1]; // v.last().unwrap().clone();       // last() borrows v.  last_element owns reference to the last element of v
-    println!("The last value is {}", last_element);
+    let last_element = v[n - 1];
     for element in v {
-        result_vec.push(last_element - element);
+        result_vec.push(1.0 - element / last_element);
     }
     result_vec // Return value
 }
